5db95b7e196a71de0c68a26609c3cc171a6f0f64
3 <168254582+3arth3@users.noreply.github.com>
2026-02-02 15:11:34 +0700

    Update NoLogging.java

--- headlessmc-auth/src/main/java/io/github/headlesshq/headlessmc/auth/NoLogging.java
+++ headlessmc-auth/src/main/java/io/github/headlesshq/headlessmc/auth/NoLogging.java
@@ -1,6 +1,5 @@
 package io.github.headlesshq.headlessmc.auth;
 
-// Bỏ chữ .logging ở giữa đi
 import net.lenni0451.commons.httpclient.IHttpClientLogger;
 
 public enum NoLogging implements IHttpClientLogger {
@@ -8,17 +7,21 @@ public enum NoLogging implements IHttpClientLogger {
 
     @Override
     public void info(String s) {
+        // NOP
     }
 
     @Override
     public void warn(String s) {
+        // NOP
     }
 
     @Override
     public void error(String s) {
+        // NOP
     }
 
     @Override
     public void error(String s, Throwable t) {
+        // NOP
     }
 }
